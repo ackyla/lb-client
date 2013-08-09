2e690829edf5552754aeb59c7ffad92bdf467ef9
アキーラ <w.ackyla@gmail.com>
2013-08-09 18:59:12 +0900

    位置情報とってみた

--- android/src/com/example/lb/MainActivity.java
+++ android/src/com/example/lb/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.lb;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements LocationListener{
+	LocationManager mLocationManager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_FINE); // 低精度
+		criteria.setPowerRequirement(Criteria.POWER_LOW); // 低消費電力
+		String provider = mLocationManager.getBestProvider(criteria, true);
+		mLocationManager.requestLocationUpdates(provider, 0, 0, this);
+		TextView tv = (TextView) findViewById(R.id.textView2);
+		tv.setText(provider);
 	}
 
 	@Override
@@ -17,6 +34,30 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		TextView tv = (TextView) findViewById(R.id.text_view_01);
+        tv.setText("Latitude:"+location.getLatitude() + ", Longitude:"+location.getLongitude());
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
