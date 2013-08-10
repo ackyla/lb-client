38fbeb151512159ae52945c51189ff38fec7fc94
アキーラ <w.ackyla@gmail.com>
2013-08-10 17:29:42 +0900

    api

---
+++ android/src/logic/base/LogicBase.java
@@ -0,0 +1,11 @@
+package logic.base;
+
+import android.content.Context;
+
+public class LogicBase {
+	protected Context context;
+	
+	public LogicBase(Context context){
+		this.context = context;
+	}
+}
