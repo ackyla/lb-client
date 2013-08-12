0931000ab4e6aabcf9e26d6f12fe9c7ed1aa63c6
アキーラ <w.ackyla@gmail.com>
2013-08-12 17:54:05 +0900

    入室情報を表示

--- android/src/com/example/lb/HomeFragment.java
+++ android/src/com/example/lb/HomeFragment.java
@@ -1,13 +1,21 @@
 package com.example.lb;
 
+import org.json.JSONObject;
+
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import dao.room.RoomEntity;
 import dao.user.UserEntity;
 import logic.user.UserLogic;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import api.API;
 
 public class HomeFragment extends Fragment {
 	
@@ -23,14 +31,22 @@ public class HomeFragment extends Fragment {
     	View v = inflater.inflate(R.layout.fragment_home, container, false);
     	
     	TextView tv1 = (TextView)v.findViewById(R.id.textView1);
-    	TextView tv2 = (TextView)v.findViewById(R.id.textView2);
+    	final TextView tv2 = (TextView)v.findViewById(R.id.textView2);
     	
     	UserLogic userLogic = new UserLogic(getActivity());
     	userEntity = userLogic.getUser();
     	
     	if(userEntity != null){
     		tv1.setText(userEntity.getName());
-    		tv2.setText("部屋ID:"+userEntity.getRoomId()+" に入室中．");
+    		API.getRoomInfo(userEntity.getRoomId(), new JsonHttpResponseHandler(){
+    			@Override
+    			public void onSuccess(JSONObject object) {
+    				Log.v("home", ""+object.toString());
+    				RoomEntity roomEntity = new RoomEntity(object);
+    				tv2.setText("入室中の部屋: "+roomEntity.getTitle());
+    			}
+    		});
+    		
     	}
     	
     	return v;
