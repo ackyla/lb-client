4566ba779b78f3d433f01089e87790bcafa953ed
phelrine <phelrine@gmail.com>
2013-08-13 18:10:04 +0900

    Add radius to hit location API

--- android/src/api/API.java
+++ android/src/api/API.java
@@ -25,14 +25,15 @@ public class API {
 	private static String getAbsoluteUrl(String relativeUrl) {
 		return URL + relativeUrl;
 	}
-	
+
 	public static void register(String name, AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("name", name);
 		post("users/create", params, handler);
 	}
-	
-	public static void postLocation(UserEntity user, Location loc, AsyncHttpResponseHandler handler) {
+
+	public static void postLocation(UserEntity user, Location loc,
+			AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("user_id", Integer.toString(user.getUserId()));
 		params.put("token", user.getToken());
@@ -40,63 +41,71 @@ public class API {
 		params.put("longitude", Double.toString(loc.getLongitude()));
 		post("locations/create", params, handler);
 	}
-	
-	public static void getUserLocations(UserEntity user, Location loc, AsyncHttpResponseHandler handler) {
+
+	public static void getUserLocations(UserEntity user, Location loc,
+			AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("user_id", Integer.toString(user.getUserId()));
 		get("users/locations", params, handler);
-	}	
-	
-	public static void createRoom(UserEntity user, String title, AsyncHttpResponseHandler handler) {
+	}
+
+	public static void createRoom(UserEntity user, String title,
+			AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("user_id", Integer.toString(user.getUserId()));
 		params.put("token", user.getToken());
 		params.put("title", title);
 		post("rooms/create", params, handler);
 	}
-	
-	public static void startGame(UserEntity user, String title, AsyncHttpResponseHandler handler) {
+
+	public static void startGame(UserEntity user, String title,
+			AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("user_id", Integer.toString(user.getUserId()));
 		params.put("token", user.getToken());
 		post("rooms/start", params, handler);
 	}
-	
-	public static void getRoomList(UserEntity user, AsyncHttpResponseHandler handler) {
+
+	public static void getRoomList(UserEntity user,
+			AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		get("rooms/list", params, handler);
 	}
-	
-	public static void enterRoom(UserEntity user, int roomId, AsyncHttpResponseHandler handler) {
+
+	public static void enterRoom(UserEntity user, int roomId,
+			AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("user_id", Integer.toString(user.getUserId()));
 		params.put("token", user.getToken());
 		params.put("room_id", Integer.toString(roomId));
 		post("rooms/enter", params, handler);
 	}
-	
+
 	public static void getRoomUsers(int roomId, AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("room_id", Integer.toString(roomId));
 		get("rooms/users", params, handler);
 	}
-	
-	public static void postHitLocation(UserEntity user, Location loc, int targetId, AsyncHttpResponseHandler handler) {
+
+	public static void postHitLocation(UserEntity user, int targetId,
+			double latitude, double longitude, double radius,
+			AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("user_id", Integer.toString(user.getUserId()));
 		params.put("token", user.getToken());
 		params.put("target_user_id", Integer.toString(targetId));
-		params.put("latitude", Double.toString(loc.getLatitude()));
-		params.put("longitude", Double.toString(loc.getLongitude()));
+		params.put("latitude", Double.toString(latitude));
+		params.put("longitude", Double.toString(longitude));
+		params.put("radius", Double.toString(radius));
 		post("room/hit", params, handler);
 	}
-	
+
 	public static void getRoomInfo(int roomId, AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("room_id", Integer.toString(roomId));
 		get("rooms/show", params, handler);
 	}
-	
+
 	public static void getUserInfo(int userId, AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("user_id", Integer.toString(userId));
