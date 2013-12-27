3aa42750f83a4444f33b4b5f93c679163253290b
phelrine <phelrine@gmail.com>
2013-12-27 17:37:23 +0900

    Drop room table

--- android/src/com/lb/api/API.java
+++ android/src/com/lb/api/API.java
@@ -44,82 +44,12 @@ public class API {
 		post("locations/create", params, handler);
 	}
 
-	public static void getRoomLocations(int roomId,
-			AsyncHttpResponseHandler handler) {
-		RequestParams params = new RequestParams();
-		params.put("room_id", Integer.toString(roomId));
-		get("locations/list", params, handler);
-	}
-
-	public static void createRoom(OldAuthEntity auth, String title, int timeLimit,
-			AsyncHttpResponseHandler handler) {
-		RequestParams params = new RequestParams();
-		params.put("user_id", Integer.toString(auth.getUserId()));
-		params.put("token", auth.getToken());
-		params.put("title", title);
-		params.put("time_limit", Integer.toString(timeLimit));
-		post("rooms/create", params, handler);
-	}
-
-	public static void startGame(OldAuthEntity auth,
-			AsyncHttpResponseHandler handler) {
-		RequestParams params = new RequestParams();
-		params.put("user_id", Integer.toString(auth.getUserId()));
-		params.put("token", auth.getToken());
-		post("users/start", params, handler);
-	}
-
-	public static void getRoomList(AsyncHttpResponseHandler handler) {
-		RequestParams params = new RequestParams();
-		get("rooms/list", params, handler);
-	}
-
-	public static void enterRoom(OldAuthEntity auth, int roomId,
-			AsyncHttpResponseHandler handler) {
-		RequestParams params = new RequestParams();
-		params.put("user_id", Integer.toString(auth.getUserId()));
-		params.put("token", auth.getToken());
-		params.put("room_id", Integer.toString(roomId));
-		post("users/enter", params, handler);
-	}
-
-	public static void getRoomUsers(int roomId, AsyncHttpResponseHandler handler) {
-		RequestParams params = new RequestParams();
-		params.put("room_id", Integer.toString(roomId));
-		get("rooms/users", params, handler);
-	}
-
-	public static void postHitLocation(OldAuthEntity auth, int targetId,
-			double latitude, double longitude, double radius,
-			AsyncHttpResponseHandler handler) {
-		RequestParams params = new RequestParams();
-		params.put("user_id", Integer.toString(auth.getUserId()));
-		params.put("token", auth.getToken());
-		params.put("target_user_id", Integer.toString(targetId));
-		params.put("latitude", Double.toString(latitude));
-		params.put("longitude", Double.toString(longitude));
-		params.put("radius", Double.toString(radius));
-		post("users/hit", params, handler);
-	}
-
-	public static void getRoomInfo(int roomId, AsyncHttpResponseHandler handler) {
-		RequestParams params = new RequestParams();
-		params.put("room_id", Integer.toString(roomId));
-		get("rooms/show", params, handler);
-	}
-
 	public static void getUserInfo(int userId, AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("user_id", Integer.toString(userId));
 		get("users/show", params, handler);
 	}
 
-	public static void getTimeLeft(int roomId, AsyncHttpResponseHandler handler) {
-		RequestParams params = new RequestParams();
-		params.put("room_id", Integer.toString(roomId));
-		post("rooms/timeleft", params, handler);
-	}
-	
 	public static void postTerritoryLocation(User user, double latitude, double longitude, double radius, AsyncHttpResponseHandler handler) {
 		RequestParams params = new RequestParams();
 		params.put("user_id", Integer.toString(user.getId()));
