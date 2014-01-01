5c95ac4240c72eb4bda5767260b8e31ec5d3da68
ackyla <w.ackyla@gmail.com>
2014-01-01 18:00:50 +0900

    カメラぐいーん

--- android/src/com/lb/ui/GameActivity.java
+++ android/src/com/lb/ui/GameActivity.java
@@ -551,7 +551,7 @@ public class GameActivity extends FragmentActivity implements ILocationUpdateSer
 			gMap.setOnMyLocationChangeListener(new OnMyLocationChangeListener(){
 				@Override
 				public void onMyLocationChange(Location location) {
-					gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15));
+					gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15));
 					gMap.setOnMyLocationChangeListener(null); // 一回移動したらリスナーを殺す
 				}
 			});
