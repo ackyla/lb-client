b57e8427271d5334837ca9014405c32c6ba00381
ackyla <w.ackyla@gmail.com>
2014-01-04 02:15:51 +0900

    くそ重いのでテリトリーリストで場所を読み込むのやめた

--- android/src/com/lb/ui/TerritoryAdapter.java
+++ android/src/com/lb/ui/TerritoryAdapter.java
@@ -31,34 +31,14 @@ public class TerritoryAdapter extends ArrayAdapter<Territory>{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		Territory item = (Territory)getItem(position);
-		if (null == convertView) convertView = mLayoutInflater.inflate(R.layout.territory_list_item, null);
-		
-    	String addressString = new String();
-    	Geocoder geocoder = new Geocoder(mContext, Locale.JAPAN);
-    	if(geocoder.isPresent()) {
-    		try {
-    			List<Address> listAddress = geocoder.getFromLocation(item.getLatitude(), item.getLongitude(), 2);
-    			if(listAddress.isEmpty()) {
-    				addressString = "住所を特定できませんでした";
-    			}else{
-    				addressString = listAddress.get(1).getAddressLine(1) + "付近";
-    			}
-    		} catch (IOException e) {
-    			e.printStackTrace();
-    		}
-    	}else{
-    		addressString = "住所を特定できませんでした";
-    	}    	
+		if (null == convertView) convertView = mLayoutInflater.inflate(R.layout.territory_list_item, null);  	
 		
 		TextView tv1 = (TextView)convertView.findViewById(R.id.tv_name);
 		tv1.setText("territory_" + item.getId());
 		
 		TextView tv2 = (TextView) convertView.findViewById(R.id.tv_radius);
 		tv2.setText("半径: " + item.getRadius()/1000 + "km");
-		
-		TextView tv3 = (TextView) convertView.findViewById(R.id.tv_location);
-    	tv3.setText("場所: " + addressString);
-		
+
 		return convertView;
 	}
 }
